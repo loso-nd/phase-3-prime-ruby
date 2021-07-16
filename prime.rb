3cf9dcedf337e2ff4e43a6f872f233313ceb3754
Nahum <losond8@gmail.com>
2021-07-16 09:21:55 -0700

    commit tree ff98ac90ea5aa03762412582166f251846bb45fd

--- prime.rb
+++ prime.rb
@@ -1,1 +1,26 @@
 # Add  code here!
+def prime?(num)
+    if num <= 1
+        false
+    elsif num == 2
+        true
+    else
+        (2..num/2).none? {|i| num % i == 0 }
+    end
+
+end
+
+
+
+
+# def prime? (n)
+#     if n <= 1
+#         false
+#     elsif n == 2
+#         true
+#     else 
+#         (2..n/2).none? { |i| n % i == 0}
+#     end
+# end
+
+# What defines an integer as a prime number? - A number / integer that is not divisible by the 2.
